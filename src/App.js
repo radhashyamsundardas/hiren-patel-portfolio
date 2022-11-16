c3e764c79fa2d918a4c4aab629997bdb02abaf34
hiren patel <hirenpatel0526@yahoo.com>
2022-11-16 14:34:03 -0800

    done with navbar js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './components/header';
 import Footer from './components/footer';
-
+import Navbar from './components/navbar'
 
 function App() {
   return (
     <div> 
-      <header/>
-      <footer/>
+      {/* <Header/> */}
+      {/* <Footer/> */}
+      <Navbar/>
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import { NavLink } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
-import {Navlink} from  'react-router-dom';
+// import {Navlink} from  'react-router-dom';
 
-function navigationBar (Props){
-return{
+export default function navbar (){
+return (
+
+    <div>
+        <navbar expand="lg" bg="light" sticky="top">
+            <NavLink className='nav-link' to="/">
+                <div class="text-dark">
+                    <h1>Hiren Patel</h1>
+                </div>
+            </NavLink>
+            <ul class="navbar-nav ml-auto navitem-indent">
+                <li class="nav-item">
+                    <NavLink to="/aboutme">
+                        <div class="nav-font text-dark">
+                            About Me
+                        </div>
+                    </NavLink>
+                </li>
+
+                <li class="nav-item">
+                    <NavLink to="/portfolio">
+                        <div class="nav-font text-dark">
+                            Portfolio
+                        </div>
+                    </NavLink>
+                </li>
+
+                <li class="nav-item">
+                    <NavLink to="/Contact">
+                        <div class="nav-font text-dark">
+                            Contact Me
+                        </div>
+                    </NavLink>
+                </li>
+
+                <li class="nav-item">
+                    <NavLink to="/resume">
+                        <div class="nav-font text-dark">
+                            Resume
+                        </div>
+                    </NavLink>
+                </li>
+            </ul>
+
+
+        </navbar>
+    </div>
+
     
+
+)
+
 }
-}
+
